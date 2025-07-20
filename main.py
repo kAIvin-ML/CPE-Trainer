9a2fa021378bb6d515b8a06a68d464266d98f9e1
kAIvin-ML <kaivin-ml@pm.me>
2025-07-20 20:52:55 +0200

    improve file validation (.txt) and sys arg validation

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import sys
-from utils import file_path_provided
+from utils import file_path_provided, is_not_empty_txt
 from data_handler import get_text_from_file
 
 
 def main():
-    if file_path_provided(sys.argv):
+
+    # Load a text file
+    if file_path_provided(sys.argv) and is_not_empty_txt(sys.argv[1]):
         text_path = sys.argv[1]
-    else:
-        raise Exception("Please try again by providing a file path.")
     text = get_text_from_file(text_path)
+    
+    # Output the content given
     print("==========BEGIN=========")
     print(text)
     print("=========END=========")
+
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,8 +1,35 @@
+import os
+from data_handler import get_text_from_file
+
 def file_path_provided(num_arguments):
     """ 
     Checks if a file path has been provided.
-    Returns True or False.
+    Returns True or an Exception.
     """
-    if len(num_arguments) < 2:
-        return False
+    if len(num_arguments) != 2:
+        raise Exception("Please try again by providing a file path as the second argument.\nExample: 'python3 main.py [path/to/file/.txt]'")
     return True
+
+
+def is_txt_file(path_to_file):
+    """
+    Checks if the provided file is a text file.
+    Returns True or an Exception.
+    """
+    ext = os.path.splitext(path_to_file)[1]
+    if ext != ".txt":
+        raise Exception("Please try again by providing a .txt file.")
+    return True
+
+
+def  is_not_empty_txt(file):
+    """
+    Checks if the .txt file is empty.
+    Returns True or an Exception.
+    """
+    if is_txt_file(file):
+        num_chars = get_text_from_file(file)
+        if len(num_chars) == 0:
+            raise Exception("The .txt file is empty. Please provide a file with content.")
+        else:
+            return True
